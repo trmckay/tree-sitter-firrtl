00a8ab53374200e90bb7dd3eac316deb42ae8d83
Trevor McKay <mail@trmckay.com>
2023-01-16 12:29:40 -0800

    passing first few tests

--- grammar.js
+++ grammar.js
@@ -1,177 +1,215 @@
 module.exports = grammar({
-  name: 'firrtl',
+  name: "firrtl",
+  word: ($) => $.identifier,
+  externals: ($) => [$._newline, $._indent, $._dedent],
   rules: {
-    word: ($) => $.identifier,
-
     circuit: ($) =>
-      optional(
-        seq(optional($.info), 'circuit', $.identifier, ':', repeat($.module))
-      ),
+      seq("circuit", $.identifier, ":", $._newline, $._indent, repeat($.module), $._dedent),
 
     module: ($) =>
-      seq(
-        optional($.info),
-        choice(
-          seq('module', $.identifier, ':', repeat($.port), $.statement),
-          seq('extmodule', $.identifier, ':', repeat($.port)),
-        )
+      choice(
+        seq(
+          "module",
+          $.identifier,
+          ":",
+          $._newline,
+          $._indent,
+          repeat($.port),
+          repeat($._statement),
+          $._dedent
+        ),
+        seq("extmodule", $.identifier, ":", $._newline, $._indent, repeat($.port), $._dedent)
       ),
 
-    port: ($) => seq(optional($.info), $.direction, $.identifier, ':', $._type),
+    port: ($) => seq($.direction, $.identifier, ":", $._type),
 
-    direction: ($) => choice('input', 'output'),
+    direction: ($) => choice("input", "output"),
 
     type_integer: ($) =>
-      seq(choice('UInt', 'SInt'), optional(seq('<', $.integer, '>'))),
+      seq(choice("UInt", "SInt"), optional(seq("<", $.integer, ">"))),
+
+    type_bundle: $ => seq("{", repeat($.field), "}"),
+
+    type_clock: $ => "Clock",
+
+    type_vector: $ => seq($._type, "[", $.integer, "]"),
 
     _type: ($) =>
       choice(
         $.type_integer,
-        seq('{', repeat($.field), '}'),
-        'clock',
-        seq($._type, '[', $.integer, ']')
+        $.type_bundle,
+        $.type_clock,
+        $.type_vector,
       ),
 
-    field: ($) => seq(optional('flip'), $.identifier, ':', $._type),
+    field: ($) => seq(optional("flip"), $.identifier, ":", $._type),
 
-    statement: ($) =>
+    wire: ($) => seq("wire", $.identifier, ":", $._type),
+
+    register: ($) => seq("reg", $.identifier, ":", $._type, ",", $._expression),
+
+    memory: ($) =>
+      seq("mem", $.identifier, ":", "(", repeat($.memory_field), ")"),
+
+    memory_field: ($) =>
+      choice(
+        seq("data-type", "=>", $._type),
+        seq("depth", "=>", $.integer),
+        seq("read-latency", "=>", $.integer),
+        seq("write-latency", "=>", $.integer),
+        seq("read-under-write", "=>", choice("old", "new", "undefined")),
+        seq("reader", "=>", $.identifier),
+        seq("writer", "=>", $.identifier),
+        seq("readwriter", "=>", $.identifier)
+      ),
+
+    instance: ($) => seq("inst", $.identifier, "of", $.identifier),
+
+    node: ($) => seq("node", $.identifier, "=", $._expression),
+
+    connect: ($) => seq($._expression, "<=", $._expression),
+
+    partial_connect: ($) => seq($._expression, "<-", $._expression),
+
+    invalidate: ($) => seq($._expression, "is", "invalid"),
+
+    conditional: ($) =>
       seq(
-        optional($.info),
-        choice(
-          seq('wire', $.identifier, ':', $._type),
-          seq(
-            'reg',
-            $.identifier,
-            ':',
-            $._type,
-            ',',
-            $.expression,
-            optional(seq($.expression, ',', $.expression))
-          ),
-          seq(
-            'mem',
-            $.identifier,
-            ':',
-            '(',
-            seq(
-              repeat(
-                choice(
-                  seq('data-_type', '=>', $._type),
-                  seq('depth', '=>', $.integer),
-                  seq('read-latency', '=>', $.integer),
-                  seq('write-latency', '=>', $.integer),
-                  seq(
-                    'read-under-write',
-                    '=>',
-                    choice('old', 'new', 'undefined')
-                  ),
-                  seq('reader', '=>', $.identifier),
-                  seq('writer', '=>', $.identifier),
-                  seq('readwriter', '=>', $.identifier)
-                )
-              )
-            ),
-            ')'
-          ),
-          seq('inst', $.identifier, 'of', $.identifier),
-          seq('node', $.identifier, '=', $.expression),
-          seq($.expression, '<=', $.expression),
-          seq($.expression, '<-', $.expression),
-          seq($.expression, 'is', 'invalid'),
-          seq(
-            'when',
-            $.expression,
-            ':',
-            $.statement,
-            optional(seq('else', ':', $.statement))
-          ),
-          seq(
-            'stop',
-            '(',
-            $.expression,
-            ',',
-            $.expression,
-            ',',
-            $.integer,
-            ')'
-          ),
-          seq(
-            'printf',
-            '(',
-            $.expression,
-            ',',
-            $.expression,
-            ',',
-            $.string,
-            repeat(seq(',', $.expression))
-          ),
-          'skip',
-          seq('(', repeat($.statement), ')')
+        "when",
+        $._expression,
+        ":",
+        $._statement_group,
+        optional(
+          seq("else", choice(seq(":", $._statement_group), $.conditional))
         )
       ),
 
-    expression: ($) =>
-      choice(
-        seq($.type_integer, '(', $.integer, ')'),
-        $.identifier,
-        seq($.expression, '.', $.identifier),
-        seq($.expression, '[', $.integer, ']'),
-        seq($.expression, '[', $.expression, ']'),
-        seq(
-          'mux',
-          '(',
-          $.expression,
-          ',',
-          $.expression,
-          ',',
-          $.expression,
-          ')'
-        ),
-        seq('validif', '(', $.expression, ',', $.expression, ')'),
-        seq($.primop, repeat($.expression), repeat($.integer))
+    stop: ($) =>
+      seq("stop", "(", $._expression, ",", $._expression, ",", $.integer, ")"),
+
+    printf: ($) =>
+      seq(
+        "printf",
+        "(",
+        $._expression,
+        ",",
+        $._expression,
+        ",",
+        $.string,
+        repeat(seq(",", $._expression)),
+        ")"
       ),
 
-    primop: ($) =>
+    empty: ($) => "skip",
+
+    _statement: ($) =>
       choice(
-        'add',
-        'sub',
-        'mul',
-        'div',
-        'mod',
-        'lt',
-        'leq',
-        'gt',
-        'geq',
-        'eq',
-        'neq',
-        'pad',
-        'asUInt',
-        'asSInt',
-        'asClock',
-        'shl',
-        'shr',
-        'dshl',
-        'dshr',
-        'cvt',
-        'neg',
-        'not',
-        'and',
-        'or',
-        'xor',
-        'andr',
-        'orr',
-        'xorr',
-        'cat',
-        'bits',
-        'head',
-        'tail'
+        $.wire,
+        $.register,
+        $.memory,
+        $.instance,
+        $.node,
+        $.connect,
+        $.partial_connect,
+        $.invalidate,
+        $.conditional,
+        $.stop,
+        $.printf,
+        $.empty
+      ),
+
+    _statement_group: ($) =>
+      choice(
+        seq(
+          "(",
+          optional(seq($._statement, repeat(seq(",", $._statement)))),
+          ")"
+        ),
+        seq($._newline, $._indent, repeat($._statement), $._dedent)
+      ),
+
+    expression_integer: ($) => seq($.type_integer, "(", $.integer, ")"),
+
+    reference: ($) => $.identifier,
+
+    subfield: ($) => seq($._expression, ".", $.identifier),
+
+    subindex: ($) => seq($._expression, "[", $.integer, "]"),
+
+    subaccess: ($) => seq($._expression, "[", $._expression, "]"),
+
+    mux: ($) =>
+      seq(
+        "mux",
+        "(",
+        $._expression,
+        ",",
+        $._expression,
+        ",",
+        $._expression,
+        ")"
+      ),
+
+    conditional_valid: ($) =>
+      seq("validif", "(", $._expression, ",", $._expression, ")"),
+
+    primop: ($) =>
+      seq(
+        choice(
+          "add",
+          "sub",
+          "mul",
+          "div",
+          "mod",
+          "lt",
+          "leq",
+          "gt",
+          "geq",
+          "eq",
+          "neq",
+          "pad",
+          "asUInt",
+          "asSInt",
+          "asClock",
+          "shl",
+          "shr",
+          "dshl",
+          "dshr",
+          "cvt",
+          "neg",
+          "not",
+          "and",
+          "or",
+          "xor",
+          "andr",
+          "orr",
+          "xorr",
+          "cat",
+          "bits",
+          "head",
+          "tail"
+        ),
+        "(",
+        repeat($._expression),
+        repeat(seq(",", $._expression)),
+        repeat(seq(",", $.integer)),
+        ")"
+      ),
+
+    _expression: ($) =>
+      choice(
+        $.expression_integer,
+        $.reference,
+        $.subfield,
+        $.subaccess,
+        $.mux,
+        $.conditional_valid,
+        $.primop
       ),
 
     string: ($) => /'[^.]*'/,
     integer: ($) => choice(/[\d]+/, /0[xX][\da-fA-F]+/),
     literal: ($) => choice($.integer, $.string),
-    identifier: ($) => /[a-zA-Z0-9_]*/,
-
-    info: ($) => seq('@', '[', $.string, ',', $.integer, ',', $.integer),
+    identifier: ($) => /[a-zA-Z_]+[a-zA-Z0-9_]*/,
   },
 });
